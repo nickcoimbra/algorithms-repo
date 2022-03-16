76b1b60afe4bdbe79013b76a11ee74d3d7b4fa0d
Nícolas Santos <nicolascoimbra2@gmail.com>
2022-03-16 11:25:34 -0300

    Add files via upload

---
+++ python/selection_sort.py
@@ -0,0 +1,20 @@
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest_index = i
+            smallest = arr[i]
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selectionSort([40, 2, 6, 34, 60, 5]))
